0fc4718768b8eeb788f75ffbe6840b9009fc4413
Stefan Boodt <S.A.Boodt@student.tudelft.nl>
2014-08-08 17:31:33 +0200

    Updated the animation class to work. Tests could be coming soon.

--- src/animations/Animation.java
+++ src/animations/Animation.java
@@ -23,7 +23,7 @@ import java.util.List;
  * </p>
  * 
  * @since 7-8-2014
- * @version 7-8-2014
+ * @version 8-8-2014
  * 
  * @see Image
  * @see List
@@ -56,6 +56,11 @@ public class Animation {
 	private long totalDuration;
 	
 	/**
+	 * Keeps track of the end times of all frames.
+	 */
+	private List<Long> endTimes;
+	
+	/**
 	 * Creates a animation.
 	 */
 	public Animation() {
@@ -67,8 +72,9 @@ public class Animation {
 	 */
 	private void setUp() {
 		totalDuration = 0;
-		currentFrameIndex = 0;
 		frames = new LinkedList<AnimeFrame>();
+		endTimes = new LinkedList<Long>();
+		start();
 	}
 	
 	/**
@@ -80,5 +86,74 @@ public class Animation {
 		AnimeFrame frame = new AnimeFrame(image, duration);
 		totalDuration += duration;
 		frames.add(frame);
+		endTimes.add(totalDuration);
+	}
+	
+	/**
+	 * (Re-)Starts the animation.
+	 */
+	public synchronized void start() {
+		animTime = 0;
+		currentFrameIndex = 0;
+	}
+	
+	/**
+	 * Update the animation to go forward by the given amount of
+	 * time. If the new time is in the time of a different frame it
+	 * updates the picture seen to the correct one.
+	 * @param time The amount of time passed since the last call to
+	 * this method. In order to have smooth animations it is
+	 * recommended that the time variable is not relatively high. The
+	 * time should be given in milliseconds.
+	 */
+	public synchronized void update(long time) {
+		if (frames.size() > 1) {
+			animTime += time;
+			if (animTime >= totalDuration) {
+				animTime = animTime % totalDuration;
+				currentFrameIndex = 0;
+			}
+			while (animTime > endTime(currentFrameIndex)) {
+				currentFrameIndex++;
+			}
+		}
+	}
+	
+	/**
+	 * Returns the time this frame ends at.
+	 * @param FrameIndex The index of the frame you want to calculate
+	 * the endtime of.
+	 * @return The time the frame at the given index ends.
+	 */
+	public synchronized final long endTime(int FrameIndex) {
+		if (endTimes.size() != frames.size()) {
+			updateEndTimes();
+		}
+		return endTimes.get(FrameIndex);
+	}
+	
+	/**
+	 * Updates all end times known to this Animation. Since this method
+	 * loops over all AnimeFrames it should be used sparingly. This
+	 * method is automatically called by {@link #endTime(int)} to
+	 * update the end times if the amount of stored times doesn't
+	 * match with the amount of AnimeFrames it must iterate over.
+	 */
+	protected synchronized final void updateEndTimes() {
+		endTimes = new LinkedList<Long>();
+		totalDuration = 0;
+		for (int i = 0; i < frames.size(); i++) {
+			totalDuration += getFrame(i).getDuration();
+			endTimes.add(totalDuration);
+		}
+	}
+	
+	/**
+	 * Returns the frame at the given index.
+	 * @param frameIndex The index you want to have.
+	 * @return The frame at the given Index.
+	 */
+	protected synchronized final AnimeFrame getFrame(int frameIndex) {
+		return frames.get(frameIndex);
 	}
 }
