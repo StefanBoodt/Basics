56dd31fa202c71c9c70ff882301342a59a05d443
Stefan Boodt <S.A.Boodt@student.tudelft.nl>
2014-08-09 11:23:55 +0200

    Started the game class.

---
+++ src/game/Game.java
@@ -0,0 +1,96 @@
+package game;
+
+/**
+ * The game class. It represents a game.
+ * 
+ * @since 9-8-2014
+ * @version 9-8-2014
+ * 
+ * @author stefanboodt
+ *
+ */
+public abstract class Game {
+
+	/**
+	 * Checks whether or not the game is paused.
+	 */
+	private boolean paused;
+	
+	/**
+	 * The time the System was last updated.
+	 */
+	private long lastTime;
+	
+	/**
+	 * Creates a new Game, and sets it pause to false.
+	 */
+	public Game() {
+		super();
+		stop();
+	}
+
+	/**
+	 * Updates the game and all its game objects. You should call
+	 * the {@link #getElapsedTime()} method to get the amount of time
+	 * that passed since the last time.
+	 */
+	public abstract void update();
+	
+	/**
+	 * Gets the elapsed time in milliSeconds.
+	 * It calls #System.currentTimeMillis() to calculate the
+	 * elapsed time and then sets it so that the next time is 
+	 * @return the elapsed time.
+	 */
+	public long getElapsedTime() {
+		long newtime = System.currentTimeMillis();
+		long elapsed = newtime - getLastTime();
+		lastTime = newtime;
+		return elapsed;
+	}
+	
+	/**
+	 * Returns the last time the time was updated.
+	 * @return The time stored in {@link #lastTime}.
+	 */
+	public long getLastTime() {
+		return lastTime;
+	}
+
+	/**
+	 * Toggles the being paused status of the game.
+	 */
+	public void pause() {
+		if (isPaused()) {
+			start();
+		}
+		else {
+			stop();
+		}
+	}
+	
+	/**
+	 * Stops the game. Updates one last time before the pause.
+	 */
+	public void stop() {
+		paused = false;
+		update();
+	}
+	
+	/**
+	 * Starts the game. Sets the last checked time to now so you get
+	 * to continue where you left of.
+	 */
+	public void start() {
+		paused = true;
+		getElapsedTime();
+	}
+	
+	/**
+	 * Gets the paused state of the game.
+	 * @return true iff the game is paused.
+	 */
+	public final synchronized boolean isPaused() {
+		return paused;
+	}
+}
---
+++ src/game/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * A package used often in gaming, although other uses are possible.
+ * 
+ * @since 9-8-2014
+ * 
+ * @author stefanboodt
+ *
+ */
+package game;
