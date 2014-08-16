e41f92293a35fea0c02438fbb3abeed7f10529d0
Stefan Boodt <S.A.Boodt@student.tudelft.nl>
2014-08-16 17:32:12 +0200

    Started with data-structures.

---
+++ src/datastructures/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * This package contains classes that collect data. The structures have
+ * different ways of storing data and different data retrieval times. The
+ * memory footprint also differs between them. Look at the classes for
+ * more information.
+ * 
+ * @since 16-8-2014
+ * 
+ * @author stefanboodt
+ *
+ */
+package datastructures;
